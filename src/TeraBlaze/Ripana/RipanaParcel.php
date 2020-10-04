e0f259d2bff3b2c3072022d47977d2a0833e0fa6
Tomiwa Ibiwoye <tomiwa@teraboxx.com>
2020-10-04 10:19:53 +0100

    Potential bug fix

--- src/TeraBlaze/Ripana/RipanaParcel.php
+++ src/TeraBlaze/Ripana/RipanaParcel.php
@@ -67,9 +67,7 @@ class RipanaParcel extends Parcel implements ParcelInterface
         $entityManager = new EntityManager($this->container->get($connectionName));
         $this->container->registerServiceInstance($entityManagerName, $entityManager);
         if ($dbConf == 'default') {
-            $this->container->setAlias('ripana.database.connection', $connectionName);
-            $this->container->setAlias('ripana.database.connection', $connectionName);
-            $this->container->setAlias('ripana.orm.entity_manager', $entityManagerName);
+            $this->container->setAlias('ripana.database.connector', $connectionName);
             $this->container->setAlias('ripana.orm.entity_manager', $entityManagerName);
         }
         Events::fire("terablaze.ripana.database.initialize.after", array($this->type, $this->options));
