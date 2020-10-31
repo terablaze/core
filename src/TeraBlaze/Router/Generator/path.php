a3d507afc67add19c089fd1f6c3dc1b9729cb39a
Tomiwa Ibiwoye <tomiwa@teraboxx.com>
2020-10-31 09:15:57 +0100

    Creates utility path() and asset() functions

---
+++ src/TeraBlaze/Router/Generator/path.php
@@ -0,0 +1,35 @@
+<?php
+
+use TeraBlaze\Container\Container;
+use TeraBlaze\Router\Router;
+
+if (!function_exists('path')) {
+    function path(string $path, array $parameters = [])
+    {
+        $container = Container::getContainer();
+        /** @var Router $router */
+        $router = $container->get(Router::class);
+        if (isset($router->getRoutes()[$path])) {
+            $path = $router->getGenerator()->generate($path, $parameters);
+        }
+        $scriptName = $_SERVER['SCRIPT_NAME'];
+        $virtualLocation = $container->hasParameter('virtualLocation') ?
+            rtrim($container->getParameter('virtualLocation'), '/\\') :
+            preg_replace('#public/[\w-]*.php(.*)$#', '', $scriptName);
+        $link = "{$virtualLocation}{$path}";
+        return htmlspecialchars($link, ENT_QUOTES);
+    }
+}
+
+if (!function_exists('asset')) {
+    function asset($uri = '')
+    {
+        $container = Container::getContainer();
+        $scriptName = $_SERVER['SCRIPT_NAME'];
+        $virtualLocation = $container->hasParameter('virtualLocation') ?
+            rtrim($container->getParameter('virtualLocation'), '/\\') :
+            preg_replace('#[\w-]*.php(.*)$#', '', $scriptName);
+        $link = "{$virtualLocation}assets/{$uri}";
+        return htmlspecialchars($link, ENT_QUOTES);
+    }
+}
