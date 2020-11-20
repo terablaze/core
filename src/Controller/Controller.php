b0c5b3c6221ca4e12629a296fe676f85521ebbc0
Tomiwa Ibiwoye <tomiwa@teraboxx.com>
2020-11-20 01:43:15 +0100

    Minor changes on abstract Controller

--- src/Controller/Controller.php
+++ src/Controller/Controller.php
@@ -152,10 +152,6 @@ abstract class Controller implements ControllerInterface
         int $status = 302,
         int $referenceType = UrlGeneratorInterface::ABSOLUTE_PATH
     ): Response {
-        $scriptName = $this->container->get('request')->getServerParams()['SCRIPT_NAME'];
-        $virtualLocation = $this->container->hasParameter('virtualLocation') ?
-            rtrim($this->container->getParameter('virtualLocation'), '/\\') :
-            preg_replace('#public/[\w-]*.php(.*)$#', '', $scriptName);
-        return $this->redirect($virtualLocation.$this->generateUrl($routeName, $parameters, $referenceType), $status);
+        return $this->redirect($this->generateUrl($routeName, $parameters, $referenceType), $status);
     }
 }
