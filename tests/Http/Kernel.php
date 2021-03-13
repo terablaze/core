f6a0ceb97a86d0890cea61237f8eb231545250ec
Tomiwa Ibiwoye <tomiwa@teraboxx.com>
2021-03-13 05:20:12 +0100

    More tests

--- tests/Http/Kernel.php
+++ tests/Http/Kernel.php
@@ -2,6 +2,14 @@
 
 namespace Tests\TeraBlaze\Http;
 
+use TeraBlaze\ErrorHandler\HtmlErrorRenderer;
+
 class Kernel extends \TeraBlaze\Core\Kernel\Kernel
 {
+    public function __construct(string $environment, bool $debug)
+    {
+        $this->environment = $environment;
+        $this->debug = $debug;
+        HtmlErrorRenderer::setTemplateDir(__DIR__);
+    }
 }
--- tests/Http/index.php
+++ tests/Http/index.php
@@ -2,7 +2,9 @@
 
 declare(strict_types=1);
 
+use Psr\Http\Message\ResponseInterface;
 use TeraBlaze\HttpBase\Request;
+use TeraBlaze\HttpBase\Response;
 
 require_once __DIR__ . "/../../vendor/autoload.php";
 
@@ -10,6 +12,7 @@ $kernel = new \Tests\TeraBlaze\Http\Kernel('dev', true);
 
 $request = Request::createFromGlobals();
 
+/** @var Response */
 $response = $kernel->handle($request);
 
 $response->send();
