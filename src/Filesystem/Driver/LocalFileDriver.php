649ba07f3e1d8e3a7c1091b14b7a2264564f6ae7
Tomiwa Ibiwoye <tomiwa@teraboxx.com>
2021-11-16 01:43:29 +0100

    Filesystem permissions

--- src/Filesystem/Driver/LocalFileDriver.php
+++ src/Filesystem/Driver/LocalFileDriver.php
@@ -4,18 +4,34 @@ namespace TeraBlaze\Filesystem\Driver;
 
 use League\Flysystem\Filesystem;
 use League\Flysystem\Local\LocalFilesystemAdapter;
+use League\Flysystem\UnixVisibility\PortableVisibilityConverter;
+use League\Flysystem\Visibility;
 
 class LocalFileDriver extends FileDriver implements FileDriverInterface
 {
     public function connect(): void
     {
-        $root = baseDir($this->config['root'] ?? '');
+        $root = $this->config['root'] ?? '';
+        if (!is_dir($root)) {
+            $root = baseDir($root);
+        }
 
-        $links = ($config['links'] ?? null) === 'skip'
+        $links = ($this->config['links'] ?? null) === 'skip'
             ? LocalFilesystemAdapter::SKIP_LINKS
             : LocalFilesystemAdapter::DISALLOW_LINKS;
 
-        $adapter = new LocalFilesystemAdapter($root, null, LOCK_EX, $links);
+        $visibility = PortableVisibilityConverter::fromArray([
+            'file' => [
+                'public' => $this->config['file']['public'] ?? 0640,
+                'private' => $this->config['file']['private'] ?? 0604,
+            ],
+            'dir' => [
+                'public' => $this->config['directory']['public'] ?? $this->config['dir']['public'] ?? 0740,
+                'private' => $this->config['directory']['private'] ?? $this->config['dir']['private'] ?? 7604,
+            ],
+        ], $this->config['visibility'] ?? Visibility::PRIVATE);
+
+        $adapter = new LocalFilesystemAdapter($root, $visibility, $this->config['lock'] ?? LOCK_EX, $links);
         $this->filesystem = new Filesystem($adapter);
     }
 }
